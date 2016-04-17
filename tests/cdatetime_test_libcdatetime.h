a1eaee5e60cae88f7929bad37a454868f4796f13
Joachim Metz <joachim.metz@gmail.com>
2016-04-17 09:45:54 +0200

    Worked on tests.

--- tests/cdatetime_test_libcdatetime.h
+++ tests/cdatetime_test_libcdatetime.h
@@ -24,10 +24,10 @@
 
 #include <common.h>
 
-/* If Cygwin libtool DLL support is enabled set LIBCDATETIME_DLL_IMPORT
- * before including libcdatetime.h
+/* If Cygwin libtool DLL support is enabled and do not want to create static
+ * executables set LIBCDATETIME_DLL_IMPORT before including libcdatetime.h
  */
-#if defined( _WIN32 ) && defined( DLL_EXPORT )
+#if defined( _WIN32 ) && defined( DLL_EXPORT ) && !defined( HAVE_STATIC_EXECUTABLES )
 #define LIBCDATETIME_DLL_IMPORT
 #endif
 
--- tests/cdatetime_test_unused.h
+++ tests/cdatetime_test_unused.h
@@ -1,5 +1,5 @@
 /*
- * The internal unused definition
+ * The unused definition
  *
  * Copyright (C) 2013-2016, Joachim Metz <joachim.metz@gmail.com>
  *
@@ -19,26 +19,32 @@
  * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if !defined( _CDATETIME_TEST_INTERNAL_UNUSED_H )
-#define _CDATETIME_TEST_INTERNAL_UNUSED_H
+#if !defined( _CDATETIME_TEST_UNUSED_H )
+#define _CDATETIME_TEST_UNUSED_H
 
 #include <common.h>
 
 #if !defined( CDATETIME_TEST_ATTRIBUTE_UNUSED )
+
 #if defined( __GNUC__ ) && __GNUC__ >= 3
 #define CDATETIME_TEST_ATTRIBUTE_UNUSED	__attribute__ ((__unused__))
+
 #else
 #define CDATETIME_TEST_ATTRIBUTE_UNUSED
-#endif
-#endif
+
+#endif /* defined( __GNUC__ ) && __GNUC__ >= 3 */
+
+#endif /* !defined( CDATETIME_TEST_ATTRIBUTE_UNUSED ) */
 
 #if defined( _MSC_VER )
 #define CDATETIME_TEST_UNREFERENCED_PARAMETER( parameter ) \
 	UNREFERENCED_PARAMETER( parameter );
+
 #else
 #define CDATETIME_TEST_UNREFERENCED_PARAMETER( parameter ) \
 	/* parameter */
-#endif
 
-#endif
+#endif /* defined( _MSC_VER ) */
+
+#endif /* !defined( _CDATETIME_TEST_UNUSED_H ) */
 
