132e82396cf9eb93f496f5e7678f815295439327
Joachim Metz <joachim.metz@gmail.com>
2026-06-21 18:13:15 +0200

    Worked on tests and changes for CI tests

--- include/libcdatetime/error.h
+++ include/libcdatetime/error.h
@@ -82,7 +82,7 @@ enum LIBCDATETIME_ARGUMENT_ERROR
 	 */
 	LIBCDATETIME_ARGUMENT_ERROR_UNSUPPORTED_VALUE		= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBCDATETIME_ARGUMENT_ERROR_CONFLICTING_VALUE		= 9
 };
@@ -214,7 +214,7 @@ enum LIBCDATETIME_OUTPUT_ERROR
 {
 	LIBCDATETIME_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBCDATETIME_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };
